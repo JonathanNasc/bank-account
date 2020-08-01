2bad35d8491ac666d1c0ed8ccce74e7e60ecec03
Jonathan Nascimento <jonathan.nasc@yahoo.com.br>
2020-08-01 13:18:01 -0300

    Delete Deposit.ts

--- src/operation/Deposit.ts
+++
@@ -1,7 +0,0 @@
-import Account from "../schemes/Account";
-import Movement from "../schemes/Movement";
-
-export let execute = (destinationId: string, amount: number):void => {
-    Account.createIfNotExist(destinationId);
-    Movement.push(destinationId, amount);
-}
